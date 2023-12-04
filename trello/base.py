2dbab4ca9dac3b412dabe7747920da4dd75b45df
thecellshepard <nfriedman@ambri.com>
2023-12-03 19:24:16 -0500

    purpose change to serve as a base framework for building new .py files

--- trello/base.py
+++ trello/base.py
@@ -1,4 +1,19 @@
-class ApiBase(object):
-    def raise_or_json(self, resp):
-        resp.raise_for_status()
-        return resp.json()
+# Copy and Paste Setup for making new classes in .py files
+
+from .api_client import APIClient
+
+REFERENCE_TARGET = 'name in url'
+
+
+class Name: # name of the REST API sectoin
+
+    def __init__(self, apikey, token):
+        self._api_client = APIClient(apikey, token)
+
+    ### Get Section ###
+
+    ### Post Section ###
+
+    ### Put Section ###
+
+    ### Delete Section ###
