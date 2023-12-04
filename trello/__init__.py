4ef435df1bb68e8455f52cf88ab4c27f5700d5bf
thecellshepard <nfriedman@ambri.com>
2023-12-03 19:58:47 -0500

    updated and cleaned for new format

--- trello/__init__.py
+++ trello/__init__.py
@@ -1,43 +1,74 @@
 from .api_client import APIClient
+from .actions import Actions
+from .boards import Boards
 from .cards import Cards
+from .checklists import Checklists
+from .customfields import CustomFields
+from .labels import Labels
+from .lists import List
+from .members import Members
+from .notifications import Notification
+from .organizations import Organizations
+from .search import Search
+from .tokens import Tokens
+from .webhooks import Webhooks
 
-"""
-class TrelloApi(object):
+
+class TrelloAPI:
     def __init__(self, apikey, token=None):
+        if not apikey or not isinstance(apikey, str):
+            raise ValueError("An API key must be provided and must be a string.")
+
+        if token is not None and not isinstance(token, str):
+            raise ValueError("If provided, the token must be a string.")
+
         self._apikey = apikey
         self._token = token
-        self.actions = Actions(apikey, token)
-        self.batches = Batches(apikey, token)
-        self.boards = Boards(apikey, token)
-        self.cards = Cards(apikey, token)
-        self.checklists = Checklists(apikey, token)
-        self.labels = Labels(apikey, token)
-        self.lists = Lists(apikey, token)
-        self.members = Members(apikey, token)
-        self.notifications = Notifications(apikey, token)
-        self.organizations = Organizations(apikey, token)
-        self.search = Search(apikey, token)
-        self.tokens = Tokens(apikey, token)
-        self.types = Types(apikey, token)
-        self.webhooks = Webhooks(apikey, token)
 
     def set_token(self, token):
         self._token = token
-        self.actions._token = token
-        self.batches._token = token
-        self.boards._token = token
-        self.cards._token = token
-        self.checklists._token = token
-        self.labels._token = token
-        self.lists._token = token
-        self.members._token = token
-        self.notifications._token = token
-        self.organizations._token = token
-        self.search._token = token
-        self.tokens._token = token
-        self.types._token = token
-        self.webhooks._token = token
-"""
 
- #   def get_token_url(self, app_name, expires='30days', write_access=True):
- #       return f"https://trello.com/1/authorize?key={self._apikey}&name={quote(app_name)}&expiration={expires}&response_type=token&scope={'read,write' if write_access else 'read'}"
+    def Actions(self):
+        return Actions(self._apikey, self._token)
+
+    def Boards(self):
+        return Boards(self._apikey, self._token)
+
+    def Cards(self):
+        return Cards(self._apikey, self._token)
+
+    def Checklists(self):
+        return Checklists(self._apikey, self._token)
+
+    def CustomFields(self):
+        return CustomFields(self._apikey, self._token)
+
+    def Labels(self):
+        return Labels(self._apikey, self._token)
+
+    def Lists(self):
+        return List(self._apikey, self._token)
+
+    def Members(self):
+        return Members(self._apikey, self._token)
+
+    def Notifications(self):
+        return Notification(self._apikey, self._token)
+
+    def Organizations(self):
+        return Organizations(self._apikey, self._token)
+
+    def Search(self):
+        return Search(self._apikey, self._token)
+
+    def Tokens(self):
+        return Tokens(self._apikey, self._token)
+
+    def Webhooks(self):
+        return Webhooks(self._apikey, self._token)
+
+    def get_token_url(self, app_name, expires='30days', write_access=True):
+        return f"https://trello.com/1/authorize?key={self._apikey}&name={app_name}&expiration={expires}&response_type=token&scope={'read,write' if write_access else 'read'}"
+
+
+__all__ = ['TrelloAPI']
